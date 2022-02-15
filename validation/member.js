a89bcd4638c0a3d140eaddea6e158b478432c155
Thejaswin S <76249670+ThejaswinS@users.noreply.github.com>
2022-02-15 19:53:43 +0530

    Update member.js

--- validation/member.js
+++ validation/member.js
@@ -1,17 +1,17 @@
 import joi from "joi";
 
-export const ValidateMemberName = (restaurantObj) => {
+export const ValidateMemberName = (memberObj) => {
     const Schema = joi.object({
         name: joi.string().required(),
     });
 
-    return Schema.validateAsync(restaurantObj);
+    return Schema.validateAsync(memberObj);
 };
 
-export const ValidateMemberSearchString = (restaurantObj) => {
+export const ValidateMemberSearchString = (memberObj) => {
     const Schema = joi.object({
         searchString: joi.string().required(),
     });
 
-    return Schema.validateAsync(restaurantObj);
+    return Schema.validateAsync(memberObj);
 };
